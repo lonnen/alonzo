65864584f3f9c08a9f16c1eb7b6c97ba92fd213d
lonnen <chris.lonnen@gmail.com>
2014-07-16 21:09:36 -0700

    rudimentary symbol parser
    introduces LispVal parsing. doesn't handle escape characters yet

--- alonzo.hs
+++ alonzo.hs
@@ -9,9 +9,49 @@ symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                          "#t" -> Bool True
+                          "#f" -> Bool False
+                          otherwise -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+-- equivalent functions:
+
+-- parseNumber = do first <- many1 digit
+--                 let d = read first
+--                 return $ Number d
+
+-- parseNumber =
+--     many1 digit >>= \first -> let d = read first in return $ Number d
+
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
