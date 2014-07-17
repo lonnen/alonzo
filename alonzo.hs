3be3fe0396bef6ae0a3856f4a30d5f07bfca8d54
lonnen <chris.lonnen@gmail.com>
2014-07-16 22:36:55 -0700

    binary numeric operator eval!
    implements binary numeric ops, so that alonzo now does simple arithmetic one variable length lists. Examples:
    
        (+ 2 2)
        (+ 2 (-4 1))
        (+ 2 (- 4 1))
        (- (+ 4 6 3) 3 5 2)

--- alonzo.hs
+++ alonzo.hs
@@ -86,3 +86,31 @@ eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+-- binary operators on numerics
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+-- weak typing of a number, which differs from R5RS spec
+-- by treating any value that can be interpreted as a number as a number
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+                          if null parsed
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
