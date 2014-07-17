385b24a051982d127bde21d9759ffb5e9c8cfd96
lonnen <chris.lonnen@gmail.com>
2014-07-16 21:16:47 -0700

    a wild eval appears!
    baby steps towards evaluating, or a really roundabout way of implementing echo
    also includes some readme beef

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 alonzo
 ------
 
-A Scheme interpreter written in Haskell in order to better learn both languages.
-
+A [Scheme](http://www.schemers.org/Documents/Standards/R5RS/HTML/) interpreter written in Haskell in order to better learn both languages. Development is mostly following Jonathan Tang's "Write yourself a Scheme" book.
 
 getting started
 ===============
--- alonzo.hs
+++ alonzo.hs
@@ -4,15 +4,15 @@ import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
-main =  getArgs >>= putStrLn . readExpr . (!! 0)
-
+main =  getArgs >>= putStrLn . show . eval . readExpr . (!! 0)
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found val"
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -64,4 +64,25 @@ parseExpr = parseAtom
         <|> parseQuoted
         <|> do char '('
                x <- (try parseList) <|> parseDottedList
+               char ')'
                return x
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ "." ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where show = showVal
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
