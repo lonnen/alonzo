ceb865c440ecb62804ecb0f0c73a488943f8da29
lonnen <chris.lonnen@gmail.com>
2014-07-16 21:05:43 -0700

    ignore spaces

--- alonzo.hs
+++ alonzo.hs
@@ -9,6 +9,9 @@ symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
