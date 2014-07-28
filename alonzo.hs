8c9b56bdd3eeaaaaede791716ecbd2ec2a9bb44a
lonnen <chris.lonnen@gmail.com>
2014-07-27 23:22:42 -0700

    add simple repl if no arguments are passed
    with this update, alonzo can handle the following:
    
        $ ./dist/build/alonzo/alonzo
        Lisp>>> (cons 2 3)                 // (2.3)
        Lisp>>> (cons 'this '())           // (this)
        Lisp>>> (cons this '())            // Unrecognized special form: this
        Lisp>>> quit

--- alonzo.hs
+++ alonzo.hs
@@ -3,13 +3,15 @@ module Main where
 import Control.Monad
 import Control.Monad.Error
 import System.Environment
+import System.IO hiding (try)
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
-main =  do
-  args <- getArgs
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+main = do args <- getArgs
+          case length args of
+              0 -> runRepl
+              1 -> evalAndPrint $ args !! 0
+              otherwise -> putStrLn "Program takes only 0 or 1 argument"
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
@@ -246,3 +248,25 @@ trapError action = catchError action (return . show)
 
 extractValue :: ThrowsError a -> a
 extractValue  (Right val) = val
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+     then return ()
+     else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
